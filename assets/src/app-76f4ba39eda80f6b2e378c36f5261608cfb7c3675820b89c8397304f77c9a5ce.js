8b681d693de14e352abe726f752f59129ec9c885
Thomas HUMMEL <thomas@hummel.link>
2023-10-18 12:16:01 +0200

    [...work in progress...]

---
+++ assets/src/app-76f4ba39eda80f6b2e378c36f5261608cfb7c3675820b89c8397304f77c9a5ce.js
@@ -0,0 +1,42 @@
+import AnchorJS from 'anchor-js'
+const anchors = new AnchorJS()
+
+export default class App {
+
+  constructor(){
+    let me = this
+    document.addEventListener("turbolinks:load", function(){
+      let sidebar = document.querySelector(".sidebar")
+      if(sidebar){
+        me.scrollSidbar(sidebar)
+        me.anchorsPage()
+        me.submenuToggle(sidebar)
+      }
+    })
+  }
+
+  scrollSidbar(sidebar){
+    let scrollTop = localStorage.getItem('scrollTop') || 0
+
+    sidebar.scrollTo({top: scrollTop})
+
+    sidebar.addEventListener('scroll', function(e) {
+      localStorage.setItem('scrollTop', sidebar.scrollTop)
+    })
+  }
+
+  anchorsPage(){
+    anchors.options = {
+      icon: "#",
+      visible: 'always'
+    };
+    anchors.add('.section > h3')
+  }
+
+  submenuToggle(sidebar){
+    document.querySelector(".submenu-toggler").addEventListener('click', function(){
+       console.log(sidebar)
+       sidebar.closest(".left-nav").classList.toggle("d-none")
+    })
+  }
+};

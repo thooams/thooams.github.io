2952b9dad983aa8615d0d8eef4dcfada5798bec8
Thomas HUMMEL <thomas@hummel.link>
2023-10-18 09:17:20 +0200

    Add Ui Bibz documentation v4.0.0.beta4

--- assets/application-ade99a4cfc0163d1d095811b31e6beb65843536f0743a843c88c6c973f036a35.js
+++
@@ -1,8 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from '@rails/ujs'
-import UiBibz from 'ui_bibz_js'
-
-Rails.start();
-
-let uiBibzJs = new UiBibz
-uiBibzJs.start();
